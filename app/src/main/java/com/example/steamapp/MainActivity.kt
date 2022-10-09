dc32b1f113cc78823921d494086bee568c35b422
Sergei <S.Senchilo@mail.ru>
2022-10-09 15:54:43 +0300

    sixth homework rep comm2 added bottom navigation

--- app/src/main/java/com/example/steamapp/MainActivity.kt
+++ app/src/main/java/com/example/steamapp/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.example.steamapp
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, BottomNavigationFragment())
+            .commit()
+    }
+}

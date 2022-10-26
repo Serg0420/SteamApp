bef1624527a3ecb30b110aa0c26f93c2968bdd81
Sergei <S.Senchilo@mail.ru>
2022-10-26 20:49:34 +0300

    seventh homework rep comm6 added service that can track users friend online status

--- app/src/main/java/com/example/steamapp/presentation/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/steamapp/presentation/ui/details/DetailsFragment.kt
@@ -1,9 +1,11 @@
 package com.example.steamapp.presentation.ui.details
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -48,11 +50,32 @@ class DetailsFragment : Fragment() {
             toolbar.setNavigationOnClickListener {
                 findNavController().navigateUp()
             }
+
+            sendCommandToService(ServiceState.START, user.steamId, user.nickName)
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+//        if (StateTrackerService.isServiceRunning) {
+//            sendCommandToService(ServiceState.STOP, "","")
+//        }
+    }
+
+    private fun sendCommandToService(
+        serviceState: ServiceState,
+        trackingID: String,
+        trackingNick: String
+    ) {
+        val intent = Intent(requireContext(), StateTrackerService::class.java)
+            .putExtra(StateTrackerService.SERVICE_STATE_COMMAND, serviceState)
+            .putExtra(StateTrackerService.TRACKING_ID, trackingID)
+            .putExtra(StateTrackerService.TRACKING_NICK, trackingNick)
+        ContextCompat.startForegroundService(requireContext(), intent)
     }
 }
---
+++ app/src/main/java/com/example/steamapp/presentation/ui/details/ServiceState.kt
@@ -0,0 +1,7 @@
+package com.example.steamapp.presentation.ui.details
+
+enum class ServiceState {
+    START,
+    PAUSE,
+    STOP
+}
